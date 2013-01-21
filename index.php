60abe59aad6c20185fc9bcf44db39ce0c9a2be16
Kater <meister_kater@yahoo.de>
2013-01-21 19:12:10 +0100

    Revert "MOVED"
    This reverts commit 1392bd580a16baf0cd622ba81c881fe1ba4f4fdb.

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+	define('indexloaded', TRUE);
+	include "includes/header.html";
+	if (file_exists("includes/install.php")) {
+		include "includes/install.php"; 
+	}
+	else {
+		include "includes/stats.php"; 
+	}
+	include "includes/foot.html";
+?>
