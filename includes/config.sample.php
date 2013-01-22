86bdd66618bdb6364aa1b329cc3608a7981f3adc
Kurim <kurim@me.com>
2013-01-22 23:11:45 +0100

    replacement for config.php

---
+++ includes/config.sample.php
@@ -0,0 +1,13 @@
+<?php
+	if(!defined('indexloaded')){die('Direct access not premitted');}
+	$webname = "DBHS";						//Titel der Webseite || Title of the Website
+	$titlebanner = "title_banner.png";		//Banner für die Webseite || Website Banner
+	$svr_name = "";							//Name of your Server
+	$svr_ip = "";							//IP of your Server
+	$svr_port = "";							//Port of your Server
+	$host = "localhost";					// MySQL Hostname
+	$db = "chernarus_1";					// MySQL Databasename
+	$user = "root";							// MySQL Username
+	$pass = "password123";					// MySQL Userpassword
+	$langselect = "de";						//Sprache (de = deutsch; en = englisch; own = eigene Sprachevorlage (config/lang/lang.php)) || Language (de = german; en = english; own = own Languagetemplate (config/lang/lang.php))
+?>
